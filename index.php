ef1dd3c53d16f9826113ff3b4cbd97fd57717da8
Raul Ramirez <raul287986@gmail.com>
2015-05-13 18:39:20 -0700

    video 5

--- index.php
+++ index.php
@@ -5,36 +5,34 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define
-define('client_ID', '1f6322580ee64ef985c108f07df45876');
-define('client_Secret', '8df27ad17af047ff84e0eb035aebc066');
+define('clientID', '1f6322580ee64ef985c108f07df45876');
+define('clientSecret', '8df27ad17af047ff84e0eb035aebc066');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
-?>
 
-<!DOCTYPE html>
+if isset(($_GET['code'])){
+	$code = ($_GET['code']);
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token_settings = array('client_id' => clientID, 
+									'client_secret' => clientSecret,
+									'grant_type' => 'authorization_code',
+									'redirect_uri' => redirectURI,
+									'code' => $code
+									);
+}
+
+?>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="description" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Untitled</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="humans.txt">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 	<!-- Creating a login for people to go and give approval for our web app to acess their Instagram Account
 	After getting approval we are now going to have the information so that we can play with it.
 	-->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
+	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
 	<script src="js/main.js"></script>
 </body>
 </html>
-
-<!--CLIENT INFO
-CLIENT ID
-1f6322580ee64ef985c108f07df45876
-CLIENT SECRET
-8df27ad17af047ff84e0eb035aebc066
-WEBSITE URL
-http://localhost/appacademyapi/index.php
-REDIRECT URI
-http://localhost/appacademyapi/index.php-->
